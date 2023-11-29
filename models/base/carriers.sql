14d271f532db7f5540f5e04166a3fce13983cdf0
Himanil Gole <himanil@5x.co>
2023-11-29 11:38:01 +0000

    This is nostradamus

--- models/base/carriers.sql
+++ models/base/carriers.sql
@@ -3,3 +3,33 @@ select
     Description as carrier_name
 from 
     {{ source('landing_zone_flights', 'carriers') }}
+
+select 
+    Code as carrier_id,
+    Description as carrier_name
+from 
+    {{ source('landing_zone_flights', 'carriers') }}
+
+select 
+    Code as carrier_id,
+    Description as carrier_name
+from 
+    {{ source('landing_zone_flights', 'carriers') }}
+
+select 
+    Code as carrier_id,
+    Description as carrier_name
+from 
+    {{ source('landing_zone_flights', 'carriers') }}
+
+select 
+    Code as carrier_id,
+    Description as carrier_name
+from 
+    {{ source('landing_zone_flights', 'carriers') }}
+
+select 
+    Code as carrier_id,
+    Description as carrier_name
+from 
+    {{ source('landing_zone_flights', 'carriers') }}
